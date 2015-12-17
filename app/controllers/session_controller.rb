7aeea3fda18d9920c4d10b756974b07c439f7536
Sylvain75 <sylvoz75@gmail.com>
2015-12-17 16:36:11 +1100

    final

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,6 +5,7 @@ class SessionController < ApplicationController
   def create
     user = User.find_by :email => params[:email]
     if user.present? && user.authenticate(params[:password])
+      
       session[:user_id] = user.id 
       redirect_to ('/twists')
     else
