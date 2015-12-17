2a938fe0b38e0f1b3c8b1225348212775e178e50
Sylvain75 <sylvoz75@gmail.com>
2015-12-18 09:16:23 +1100

    signup fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,6 @@ class User < ActiveRecord::Base
                                    dependent:   :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :email, :presence => true, :uniqueness => true
 end
